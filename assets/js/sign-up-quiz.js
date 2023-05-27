7aadd11616d265dd45b953a14a28134bf3e0d393
mohanjemima <JemimaNMohan@outlook.com>
2023-05-27 22:57:22 +0100

    Fixed recommendation tag function

--- assets/js/sign-up-quiz.js
+++ assets/js/sign-up-quiz.js
@@ -1,65 +1,78 @@
-let htmlValue = undefined;
-let cssValue = undefined;
+let htmlChoice ;
+let cssChoice ;
+let text;
+
 
 function update(elementName){
-    let value = getRadioValue(elementName);
-    if(elementName == "css-skill"){
-        cssValue = value;
+    let choice = getSelectedRadio(elementName);
+
+    if(elementName == 'css-skill'){
+        cssChoice = choice;
     }else if(elementName== 'html-skill'){
-        htmlValue = value;
+        htmlChoice = choice;
     }
-    console.log(htmlValue+"  |  "+cssValue);
 
+    text = "We recommend ";
 
-    if(htmlValue != undefined && cssValue != undefined) {
-        setRecommendation()
+    if(htmlChoice != undefined && cssChoice != undefined){
+        setRecommendation();
+        console.log(text);
+        display();
     }
+
 }
- function getRadioValue(elementName){
+
+ function getSelectedRadio(elementName) {
      let radios = document.getElementsByName(elementName);
      for (let i = 0, length = radios.length; i < length; i++) {
          if (radios[i].checked) {
-             return radios[i].value;
+             return radios[i].id;
          }
      }
+ }
+
+function setRecommendation() {
+
+    if (htmlChoice == "html-option-1" && cssChoice == "css-option-1") {
+        text += "starting the whole course from the beginning.";
+    } else {
+        switch (htmlChoice) {
+            case "html-option-1":
+                text += "starting the HTML chapter from the beginning ";
+                break;
+            case "html-option-2":
+                text += "skipping the HTML introduction and starting with the articles ";
+                break;
+            case "html-option-3":
+                text += "doing the HTML Quiz and reading the chapter Summary ";
+                break;
+            case "html-option-4":
+                text += "reading the HTML chapter Summary ";
+                break;
+        }
+
+        switch (cssChoice) {
+            case "css-option-1":
+                text += "and starting the CSS chapter from the beginning.";
+                break;
+            case "css-option-2":
+                text += "and skipping the CSS introduction and starting with the articles";
+                break;
+            case "css-option-3":
+                text += "and doing the CSS Quiz and reading the chapter Summary ";
+                break;
+            case "css-option-4":
+                text += "and reading the CSS chapter Summary.";
+                break;
+        }
+
+    }
 }
 
-function setRecommendation(){
-    let text="";
-    if(cssValue == 0 && htmlValue == 0){
-        text = "We recommend starting the whole course from the beginning.";
-    }else{
-        switch (htmlValue) {
-            case 0:
-                text+= "and starting the HTML chapter from the beginning.";
-                break;
-            case 1:
-                text+= "and skipping the HTML introduction and starting with the articles";
-                break;
-            case 2:
-                text+= ", doing the HTML Quiz and reading the chapter Summary ";
-                break;
-            case 3:
-                text+= "and reading the HTML chapter Summary.";
-                break;
-        }
-
-        switch (cssValue) {
-            case 0:
-                text+= "and starting the CSS chapter from the beginning.";
-            break;
-            case 1:
-                text+= "and skipping the CSS introduction and starting with the articles";
-                break;
-            case 2:
-                text+= ", doing the CSS Quiz and reading the chapter Summary ";
-                break;
-            case 3:
-                text+= "and reading the CSS chapter Summary.";
-                break;
-        }
-    }
-    document.getElementsByClassName('notification-box-text').innerText = text;
+function display(){
+    document.getElementById(htmlChoice).checked = true;
+    document.getElementById(cssChoice).checked = true;
+    document.getElementById('recommendation-text').textContent = text;
     document.getElementById('notification-box').style.visibility = "visible";
 
 
