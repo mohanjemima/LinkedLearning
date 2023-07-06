88698cda6ceae6936d538d505d6ed5d026dabf00
mohanjemima <JemimaNMohan@outlook.com>
2023-07-06 21:48:07 +0100

    improved security

--- util/connection.php
+++ util/connection.php
@@ -1,6 +1,6 @@
 <?php
-$hostname = "devweb2022.cis.strath.ac.uk";
 // DONT COMMIT YOUR VALUES TO VCS!!!
+$hostname = "devweb2022.cis.strath.ac.uk";
 $username = "rqb22133";
 $password = "ooraub0Emah2";
 $db = "rqb22133";
@@ -8,5 +8,18 @@ $db = "rqb22133";
 $conn = new mysqli($hostname, $username, $password, $db);
 
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Disable error display
+error_reporting(0);
+
+// Set up secure connection options
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$conn->set_charset("utf8mb4");
+$conn->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+
+// Validate the connection status
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
--- util/fetch.php
+++ util/fetch.php
@@ -1,7 +1,8 @@
 <?php
 include 'connection.php';
 
-function get_associative_result_list($result) {
+function get_associative_result_list($result): array
+{
     $rows = array();
     while ($row = $result->fetch_assoc()) {
         $rows[] = $row;
@@ -9,22 +10,25 @@ function get_associative_result_list($result) {
     return $rows;
 }
 
-function fetch($query) {
+function fetch($query): array
+{
     global $conn;
-    $result = $conn->query("SELECT * FROM $query;");
+    $stmt = $conn->prepare("SELECT * FROM $query");
+    $stmt->execute();
+    $result = $stmt->get_result();
     return get_associative_result_list($result);
 }
 
-function list_all_content() {
-    // store under lesson keys to enable merging then conversely sorting of items
-    $lessons = array_reduce(get_lessons(), function($result, $item) {
+function list_all_content()
+{
+    $lessons = array_reduce(get_lessons(), function ($result, $item) {
         $result[strval($item["id"] + 0.1)] = $item;
         return $result;
     }, array());
-    $quizzes = array_reduce(get_quizzes(), function($result, $item) {
+    $quizzes = array_reduce(get_quizzes(), function ($result, $item) {
         $count = 0.2;
-        foreach(array_keys($result) as $key) {
-            if(strpos($key, strval($item["lesson_id"])) > -1){
+        foreach (array_keys($result) as $key) {
+            if (str_contains($key, strval($item["lesson_id"]))) {
                 $count += 0.1;
             }
         }
@@ -38,44 +42,157 @@ function list_all_content() {
     return array_values($list);
 }
 
-function get_lessons() {
-    return fetch("Lesson");
-}
-
-function get_lesson($lesson_id) {
+function get_lessons(): array
+{
     global $conn;
-    $lesson_id = mysqli_real_escape_string($conn, $lesson_id);
-    return fetch("Lesson WHERE id=$lesson_id");
+    $stmt = $conn->prepare("SELECT * FROM Lesson");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return get_associative_result_list($result);
 }
 
-function get_demo_items($lesson_id) {
+function get_lesson($lesson_id): array
+{
     global $conn;
-    $lesson_id = mysqli_real_escape_string($conn, $lesson_id);
-    return fetch("DemoItem WHERE lesson_id=$lesson_id");
+    $stmt = $conn->prepare("SELECT * FROM Lesson WHERE id = ?");
+    $stmt->bind_param("i", $lesson_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return get_associative_result_list($result);
 }
 
-function get_quizzes() {
-    return fetch("Quiz");
-}
-
-function get_quiz($quiz_id) {
+function get_demo_items($lesson_id): array
+{
     global $conn;
-    $quiz_id = mysqli_real_escape_string($conn, $quiz_id);
-    return fetch("Quiz WHERE id=$quiz_id");
+    $stmt = $conn->prepare("SELECT * FROM DemoItem WHERE lesson_id = ?");
+    $stmt->bind_param("i", $lesson_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return get_associative_result_list($result);
 }
 
-function get_quiz_questions($quiz_id) {
+function get_quizzes(): array
+{
     global $conn;
-    $quiz_id = mysqli_real_escape_string($conn, $quiz_id);
-    $questions = fetch("QuizQuestion WHERE quiz_id=$quiz_id");
-    for ($i = 0; $i < count($questions); $i++) {
-        $questions[$i]["answers"] = get_quiz_question_answers($questions[$i]["id"]);
+    $stmt = $conn->prepare("SELECT * FROM Quiz");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return get_associative_result_list($result);
+}
+
+function get_quiz($quiz_id): array
+{
+    global $conn;
+    $stmt = $conn->prepare("SELECT * FROM Quiz WHERE id = ?");
+    $stmt->bind_param("i", $quiz_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return get_associative_result_list($result);
+}
+
+function get_quiz_questions($quiz_id): array
+{
+    global $conn;
+    $stmt = $conn->prepare("SELECT * FROM QuizQuestion WHERE quiz_id = ?");
+    $stmt->bind_param("i", $quiz_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $questions = get_associative_result_list($result);
+
+    foreach ($questions as &$question) {
+        $question["answers"] = get_quiz_question_answers($question["id"]);
     }
+
     return $questions;
 }
 
-function get_quiz_question_answers($quiz_question_id) {
+function get_quiz_question_answers($quiz_question_id): array
+{
     global $conn;
-    $quiz_id = mysqli_real_escape_string($conn, $quiz_question_id);
-    return fetch("QuizAnswer WHERE quiz_question_id=$quiz_question_id");
+    $stmt = $conn->prepare("SELECT * FROM QuizAnswer WHERE quiz_question_id = ?");
+    $stmt->bind_param("i", $quiz_question_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return get_associative_result_list($result);
 }
+
+//include 'connection.php';
+//
+//function get_associative_result_list($result) {
+//    $rows = array();
+//    while ($row = $result->fetch_assoc()) {
+//        $rows[] = $row;
+//    }
+//    return $rows;
+//}
+//
+//function fetch($query) {
+//    global $conn;
+//    $result = $conn->query("SELECT * FROM $query;");
+//    return get_associative_result_list($result);
+//}
+//
+//function list_all_content() {
+//    // store under lesson keys to enable merging then conversely sorting of items
+//    $lessons = array_reduce(get_lessons(), function($result, $item) {
+//        $result[strval($item["id"] + 0.1)] = $item;
+//        return $result;
+//    }, array());
+//    $quizzes = array_reduce(get_quizzes(), function($result, $item) {
+//        $count = 0.2;
+//        foreach(array_keys($result) as $key) {
+//            if(strpos($key, strval($item["lesson_id"])) > -1){
+//                $count += 0.1;
+//            }
+//        }
+//
+//        $result[strval($item["lesson_id"] + $count)] = $item;
+//        return $result;
+//    }, array());
+//
+//    $list = $lessons + $quizzes;
+//    ksort($list);
+//    return array_values($list);
+//}
+//
+//function get_lessons() {
+//    return fetch("Lesson");
+//}
+//
+//function get_lesson($lesson_id) {
+//    global $conn;
+//    $lesson_id = mysqli_real_escape_string($conn, $lesson_id);
+//    return fetch("Lesson WHERE id=$lesson_id");
+//}
+//
+//function get_demo_items($lesson_id) {
+//    global $conn;
+//    $lesson_id = mysqli_real_escape_string($conn, $lesson_id);
+//    return fetch("DemoItem WHERE lesson_id=$lesson_id");
+//}
+//
+//function get_quizzes() {
+//    return fetch("Quiz");
+//}
+//
+//function get_quiz($quiz_id) {
+//    global $conn;
+//    $quiz_id = mysqli_real_escape_string($conn, $quiz_id);
+//    return fetch("Quiz WHERE id=$quiz_id");
+//}
+//
+//function get_quiz_questions($quiz_id) {
+//    global $conn;
+//    $quiz_id = mysqli_real_escape_string($conn, $quiz_id);
+//    $questions = fetch("QuizQuestion WHERE quiz_id=$quiz_id");
+//    for ($i = 0; $i < count($questions); $i++) {
+//        $questions[$i]["answers"] = get_quiz_question_answers($questions[$i]["id"]);
+//    }
+//    return $questions;
+//}
+//
+//function get_quiz_question_answers($quiz_question_id) {
+//    global $conn;
+//    $quiz_id = mysqli_real_escape_string($conn, $quiz_question_id);
+//    return fetch("QuizAnswer WHERE quiz_question_id=$quiz_question_id");
+//}
